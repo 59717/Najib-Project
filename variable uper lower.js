0a840fb264c49cba5021fdeddf1f8d0e3e9be3fa
najieb <najieb.el57@smk.belajar.id>
2025-01-30 13:06:24 +0700

    Add files via upload

---
+++ variable uper lower.js
@@ -0,0 +1,18 @@
+
+const teks = 'Ngantuk men';
+const hurufKecil = teks.toLowerCase(); // Mengubah menjadi huruf kecil
+const hurufBesar = teks.toUpperCase(); // Mengubah menjadi huruf besar
+console.log(hurufKecil); // Output: 'ngantuk men'
+console.log(hurufBesar); // Output: 'NGANTUK MEN
+const nama = 'sarip';
+console.log(nama[0]); // s
+
+nama[0] = 'S';
+console.log(nama); // saripconst nama = 'sarip';
+console.log(nama[0]); // s
+
+nama[0] = 'S';
+console.log(nama); // sarip
+
+const b = 9007199254740992n;
+console.log(b +  5n); // 9007199254740997n
---
+++ variable.js
@@ -0,0 +1,11 @@
+const siswa =['rafa','najieb','rafka'];
+siswa[1] ='nopal';
+console.log(siswa);
+
+
+console.log['hello word'];
+
+
+const teks = 'Dunia sedang, sangat baik baik saja';
+const teksBaru = teks.replace('sangat', 'Tidak'); // Mengganti 'Dunia sadang'dengan'Tidak baik baik saja';
+console.log(teksBaru); // Output: 'Dunia sedang, Tidak baik baik saja';
